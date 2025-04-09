ba9e5ff1c466c1bb54ac8865cf9aa96fbda05f6d
Полина Жердева <zherdevap00@mail.ru>
2025-04-10 02:07:56 +0700

    3 напряжения питания (6, 7, 8)В

--- с флешки/power_supply.cpp
+++ с флешки/power_supply.cpp
@@ -21,25 +21,19 @@ int main()
 	ViSession defaultRM, vi;
 	char buf[256] = { 0 };
 	long ErrorStatus;
+	char command[256];
+
 
 	float I_offset = 0.36; // A
-	float found_U_offset = NAN; // found offset voltage U 
-
-	//output 1
-	float limitI_offset = 20; //mA
-	float limitU_offset = 1; //V
-	//output 2
-	float limitI_supply = 500; //mA
-	float limitU_supply = 6; //V
-	//float number;
-	float measured_current = 0;
-	float measured_voltage = 0;
-	char command[256];
 	
+	float limitI_offset = 20; //mA (output 1)
+	float limitI_supply = 500; //mA (output 2)
+
+	float found_U_offsets[3] = { NAN, NAN, NAN }; // (output 1)
+	float limitU_supplies[] = { 6, 7, 8 }; //V (output 2)
+
 	double step = 0.1;
-    bool stepChanged = false;
-	//ViStatus status;
-	//status = viOpenDefaultRM(&defaultRM);
+    
 
 	viOpenDefaultRM(&defaultRM);
 
@@ -52,86 +46,24 @@ int main()
 	ErrorStatus = viPrintf(vi, (ViPRsrc)"*IDN?\n");
 	ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
 	printf("Instrument identification string: %s\n", buf);
-
-	ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out1\n");
-	//ErrorStatus = viPrintf(vi, (ViPRsrc)"VOLT 1\n");
-	//ErrorStatus = viPrintf(vi, (ViPRsrc)"CURR 200 mA\n");
-	snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset);
-	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
-	ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:VOLT?\n");
-	ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
-	measured_voltage = atof(buf);
-	if (measured_voltage < limitU_offset) {
-		limitU_offset += limitU_offset - measured_voltage;
-		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset);
-		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
-	}
 	
-	snprintf(command, sizeof(command), "CURR %.2f mA\n", limitI_offset);
-	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+	for (int i=0; i<3; i++) {
+		float limitU_supply = limitU_supplies[i];
+		float limitU_offset = 1; //V
+		float found_U_offset = NAN;
+        float measured_current = 0;
+        float measured_voltage = 0;
+		double step_local = step;
+		bool stepChanged = false;
 
-	ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
-	//ErrorStatus = viPrintf(vi, (ViPRsrc)"VOLT 5\n");
-	//ErrorStatus = viPrintf(vi, (ViPRsrc)"CURR 100 mA\n");
-	snprintf(command, sizeof(command), "CURR %.2f mA\n", limitI_supply);
-	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+		printf("===== Starting for limitU_supply = %.2f V =====\n", limitU_supply);
 
-	ErrorStatus = viPrintf(vi, (ViPRsrc)"OUTP ON\n");
-
-	ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
-	snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_supply);
-	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
-	ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:VOLT?\n");
-	ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
-	measured_voltage = atof(buf);
-	if (measured_voltage < limitU_supply) {
-		limitU_supply += limitU_supply - measured_voltage;
-		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_supply);
-		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
-	}
-
-	while (1) {
-		
-		//std::this_thread::sleep_for(std::chrono::seconds(2));
-
-		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
-		ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:CURR?\n");
-		ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
-		printf("Current = %s\n", buf);
-		measured_current = atof(buf);
-		if ((measured_current <= I_offset + 0.01) && (measured_current >= I_offset - 0.01)) {
-			found_U_offset = limitU_offset;
-			printf("Last current = %f\n", measured_current);
-			printf("Found offset voltage = %f\n", found_U_offset);
-			break;
-		}
-		else if ((stepChanged)&& (measured_current > I_offset + 0.01)) {
-			found_U_offset = limitU_offset;
-			printf("The value of voltage is out of range 350-370 mA. The nearest greater value was found");
-			printf("Last current = %f\n", measured_current);
-			printf("Found offset voltage = %f\n", found_U_offset);
-			break;
-		}
-		
-		//std::this_thread::sleep_for(std::chrono::seconds(2));
-
-		limitU_offset -= step; //0.1;
-
-		if ((!stepChanged) && (limitU_offset > (I_offset + 0.01))) {
-            limitU_offset += step;
-			step = 0.01;
-            stepChanged = true;
-		}
-		
-		if (limitU_offset == 0.4) {
-			printf("Last current = %f out of 0.4 voltage\n", measured_current);
-			break;
-		}
-
+		// Output 1 - set I + U
 		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out1\n");
 		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset);
 		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
-		
+
+
 		ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:VOLT?\n");
 		ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
 		measured_voltage = atof(buf);
@@ -140,8 +72,92 @@ int main()
 			snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset);
 			ErrorStatus = viPrintf(vi, (ViPRsrc)command);
 		}
-		
+		snprintf(command, sizeof(command), "CURR %.2f mA\n", limitI_offset);
+		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+		// Output 2 - set I
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+		snprintf(command, sizeof(command), "CURR %.2f mA\n", limitI_supply);
+		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+		// Outputs ON
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"OUTP ON\n");
+
+		// Output 2 - set U
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_supply);
+		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+		// add value if U need
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:VOLT?\n");
+		ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
+		measured_voltage = atof(buf);
+		if (measured_voltage < limitU_supply) {
+			limitU_supply += limitU_supply - measured_voltage;
+			snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_supply);
+			ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+		}
+
+		while (1) {
+			
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+
+			ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+			ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:CURR?\n");
+			ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
+			printf("Current = %s\n", buf);
+			measured_current = atof(buf);
+			if ((measured_current <= I_offset + 0.01) && (measured_current >= I_offset - 0.01)) {
+				found_U_offset = limitU_offset;
+				printf("Match found: I = %f A\n", measured_current);
+				printf("Found offset voltage = %f\n", found_U_offset);
+				break;
+			}
+			else if ((stepChanged)&& (measured_current > I_offset + 0.01)) {
+				found_U_offset = limitU_offset;
+				printf("The value of voltage is out of range 350-370 mA. The nearest greater value was found\n");
+				printf("I = %f A\n", measured_current);
+				printf("Found offset voltage = %f\n", found_U_offset);
+				break;
+			}
+			
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+
+			limitU_offset -= step_local; //0.1;
+
+			if ((!stepChanged) && (limitU_offset > (I_offset + 0.01))) {
+				limitU_offset += step_local;
+				step_local = 0.01;
+				stepChanged = true;
+			}
+			
+			if (limitU_offset <= 0.4) {
+				printf("Reached voltage floor. I = %f A\n\n", measured_current);
+				break;
+			}
+
+			ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out1\n");
+			snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset);
+			ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+			
+			ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:VOLT?\n");
+			ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
+			measured_voltage = atof(buf);
+			if (measured_voltage < limitU_offset) {
+				limitU_offset += limitU_offset - measured_voltage;
+				snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset);
+				ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+			}
+			
+		}
+
+		found_U_offsets[i] = found_U_offset;
 	}
+	
+	// Show results
+    printf("======== Final Results ========\n");
+    for (int i = 0; i < 3; ++i) {
+        printf("limitU_supply = %.2f V -> found_U_offset = %.3f V\n", limitU_supplies[i], found_U_offsets[i]);
+    }
 
 	viClose(vi);
 	viClose(defaultRM);
