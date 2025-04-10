b7afec04f3e9afef927ac642b21ceb0c7f3e8305
aatro88 <tro8anya@gmail.com>
2025-04-10 16:42:54 +0700

    Добавила два cpp за сегодня

---
+++ с флешки/panorama/test_panorama/test_panorama(10.04.2025).cpp
@@ -0,0 +1,270 @@
+﻿// PanoramaCppExample.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+//#include "stdafx.h"
+#include <conio.h>
+#include <stdlib.h>
+#include "visa.h"
+#include "windows.h"
+#include <string>
+#include <chrono>
+#include <thread>
+
+#pragma comment(lib, "E:\\MicranStart\\SCPI_R4M\\_Libs\\mivisa32.lib")
+//#pragma comment(linker, "/include:E:\\MicranStart\\SCPI_R4M\\_Libs\\mivisa32.lib")
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	ViSession rm, rsrc;
+	ViStatus status;
+	status = viOpenDefaultRM(&rm);
+	char buff[128];
+
+	char command[256] = { 0 };
+
+	//char cmd_str[128];
+	// Open conection
+	//strcpy(cmd_str, "TCPIP::localhost::8888::SOCKET::VNA");
+	status = viOpen(rm, (ViString)"TCPIP::169.254.0.254::8888::SOCKET::VNA", VI_EXCLUSIVE_LOCK, 10000, &rsrc); // directly (via MiVISA only) without Micran Instrument Connector	
+	//status = viOpen(rm, "TCPIP::localhost::5025::SOCKET", VI_EXCLUSIVE_LOCK, 10000, &rsrc); // via arbitrary VISA library using Micran Instrument Connector
+
+	//путь до файла конфигурации
+	const char* config_file_path = "E:\\MicranStart\\kal.R4MC";
+
+
+	if (status != VI_SUCCESS)
+	{
+		viStatusDesc(rm, status, buff); // Get status description
+		printf("viOpen: %s\r\n", buff);
+		std::cin.get();
+		return 0;
+	}
+	// Set timeout
+	viSetAttribute(rsrc, VI_ATTR_TMO_VALUE, 5000);
+	// Send parameters
+	// Device ID
+	//strcpy(cmd_str, "*IDN?\n");
+	status = viQueryf(rsrc, (ViString)"*IDN?\n", (ViString)"%T", buff);
+	printf("IDN: %s\r\n", buff);
+	// Send "Reset" command
+	status = viPrintf(rsrc, (ViString)"*RST\r\n");
+	// Send "Clear status" command
+	status = viPrintf(rsrc, (ViString)"*CLS\r\n");
+	// Get options
+	//status = viQueryf(rsrc, "SYSTem:MCLass:CATalog?\n", "%T", buff);
+	//printf("Options: %s\r\n", buff);
+	//status = viPrintf(rsrc, "SYSTem:PRESet\n");
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+
+	// загрузка файла конфигурации
+	/*snprintf(command, sizeof(command), "MMEMory:LOAD \"%s\"\n", config_file_path);
+	status = viPrintf(rsrc, (ViPRsrc)command);*/
+
+	status = viPrintf(rsrc, (ViString)":MMEMory:LOAD:CORRection \"E:\\MicranStart\\kal.r4mc\"\n");
+	status = viPrintf(rsrc, (ViString)":SENSe:CORRection ON\n");
+
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+
+	printf("Error: %s\r\n", buff);
+	
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	ViUInt32 retCnt = 0;
+	
+
+	// Set RBW
+	status = viPrintf(rsrc, (ViString)":SENSe:BANDwidth %d\n", 10000);
+	// Set power level value
+	status = viPrintf(rsrc, (ViString)"SOURce:POWer -10\n");
+
+	
+
+	// Set frequency range values
+	double start = 40, stop = 20000;
+	status = viPrintf(rsrc, (ViString)"SENSe:FREQuency:STARt %lfMHz;STOP %lfMHz\n", start, stop);
+	// Set points count
+	int points = 201;
+	status = viPrintf(rsrc, (ViString)"SENSe:SWEEp:POINts %d\n", points);
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+
+	// Port 1 input attenuator value
+	int att = 20;
+	status = viPrintf(rsrc, (ViString)"SENSe:POWer:ATTenuator ARECeiver,%d\n", att);
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+
+	
+
+	// Sync gen parameters
+	status = viPrintf(rsrc, (ViString)"SENSe:PULSe1:PERiod 110us\n");
+	status = viPrintf(rsrc, (ViString)"SENSe:PULSe1:WIDTh 10us\n");
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+
+	// Disable continous sweeping
+	status = viPrintf(rsrc, (ViString)"INITiate:CONTinuous OFF\n");
+	// Set frequency sweep mode
+	status = viPrintf(rsrc, (ViString)":SENSe:SWEep:TYPE LINear\n");
+	// Trigger RUN
+	status = viPrintf(rsrc, (ViString)":INITiate:IMMediate\n");
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+
+	// Checking Markers
+	// Set diagram active
+	status = viPrintf(rsrc, (ViString)"DISPlay:WINDow:ACTivate\n");
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+	// Turn on marker
+	status = viPrintf(rsrc, (ViString)":CALCulate:MARKer0:STATe ON\n");
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+	// Set marker on frequency
+	//status = viPrintf(rsrc, ":CALCulate:MARKer0:X 3GHz\n");
+	// Set marker search user domain
+	status = viPrintf(rsrc, (ViString)":CALCulate:MARKer0:FUNCtion:DOMain:USER:RANGe 1\n"); // set 0 if full span needed
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+	status = viPrintf(rsrc, (ViString)":CALCulate:MARKer0:FUNCtion:DOMain:USER:STARt 1GHz\n");
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+	status = viPrintf(rsrc, (ViString)":CALCulate:MARKer0:FUNCtion:DOMain:USER:STOP 2GHz\n");
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+	// Set marker on maximum
+	status = viPrintf(rsrc, (ViString)":CALCulate:MARKer0:FUNCtion:SELect MAXimum\n");
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+
+	
+// Wait for measurement complete
+	status = viQueryf(rsrc, (ViString)"*OPC?\n", (ViString)"%T", buff);
+
+
+	if (status == VI_SUCCESS)
+	{
+		printf("Data ready\r\n");
+		status = viPrintf(rsrc, (ViString)":CALCulate:PARameter:SELect \"Trc_S11\"\n");
+		status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+		printf("Error: %s\r\n", buff);
+		// Set data format		
+		status = viPrintf(rsrc, (ViString)":FORMat REAL,64\n");
+		status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+		printf("Error: %s\r\n", buff);
+		bool bSData = true;
+		// Query data
+		if (bSData)
+			status = viPrintf(rsrc, (ViString)":CALCulate:DATA? SDATA\n");
+		else
+			status = viPrintf(rsrc, (ViString)":CALCulate:DATA? FDATA\n");
+
+		// Parse IEEE 488.2 binary block	
+		int bitSize = 0;
+		char head[32];
+		if ((status = viRead(rsrc, (ViPBuf)buff, 2, &retCnt)) < 0) // #n - number of bytes of size record
+			return 0;
+		buff[retCnt] = '\0';
+		memcpy(&head[0], &buff, retCnt);
+		bitSize = retCnt;
+		int numBytes = atoi(buff + 1);
+		if (buff[0] != '#' || numBytes == 0)
+			return 0;
+
+		if (viRead(rsrc, (ViPBuf)buff, numBytes, &retCnt) < 0) // read data length
+			return 0;
+		buff[retCnt] = '\0';
+		memcpy(&head[0] + bitSize, &buff, retCnt);
+		bitSize += retCnt;
+		head[bitSize] = '\0';
+		int dataSize = atoi(buff);
+		if (dataSize == 0)
+			return 0;
+
+		double* data = new double[dataSize + 1];
+		if (viRead(rsrc, (ViPBuf)data, dataSize + 1, &retCnt) < 0)
+			return 0;
+
+		// Writing data to memory trace
+		if (!bSData) // supports only formatted data 
+		{
+			status = viPrintf(rsrc, (ViString)"*CLS\r\n");
+			status = viPrintf(rsrc, (ViString)"MMEMory:CREate \"Mem1\",S11\n");
+			status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+			printf("Error: %s\r\n", buff);
+			status = viPrintf(rsrc, (ViString)":CALCulate:PARameter:SELect \"Mem1\"\n");
+			status = viPrintf(rsrc, (ViString)":CALCulate:FORMat MLOGarithmic\n");
+			status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+			printf("Error: %s\r\n", buff);
+			status = viPrintf(rsrc, (ViString)":CALCulate:DATA FMEM,%s", head);
+			((ViPBuf)data)[retCnt - 1] = '\n';
+			status = viWrite(rsrc, (ViPBuf)data, retCnt, &retCnt);
+		}
+		for (int i = 0; i < points; i++)
+		{
+			if (bSData)
+				printf("%g MHz\tRe: %f\tIm: %f\r\n", start + i * (stop - start) / (points - 1), data[i * 2], data[i * 2 + 1]);
+			else
+				printf("%g MHz\tRe: %f\t\r\n", start + i * (stop - start) / (points - 1), data[i]);
+		}
+		delete[] data;
+	}
+	else
+	{
+		printf("Data not ready. OPC failed\r\n");
+		status = viPrintf(rsrc, (ViString)"abort\n");
+
+		// Check errors
+		status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+		printf("Error: %s\r\n", buff);
+	}
+	// Get marker value
+	status = viQueryf(rsrc, (ViString)"CALCulate:MARKer0:Y? \"Trc_S11\"\n", (ViString)"%T", buff);
+	printf("Marker #0 value: %s\r\n", buff);
+	// Check errors
+	status = viQueryf(rsrc, (ViString)"SYSTem:ERRor?\n", (ViString)"%T", buff);
+	printf("Error: %s\r\n", buff);
+	// Hide all markers
+	status = viPrintf(rsrc, (ViString)"CALCulate:MARKer:AOFF\n");
+	// Wait for user action
+	/*_getch();
+	// Clean up averaging
+	status = viPrintf(rsrc, "SENSe:AVERage:CLEar\n");
+	status = viQueryf(rsrc, "SYSTem:ERRor?\n", "%T", buff);
+	printf("Error: %s\r\n", buff);*/
+
+	// Wait for user action
+	std::cin.get();
+
+	// Stop measurement for automatic mode
+	//status = viPrintf(rsrc, ":ABORt\n");
+	// Close session
+	status = viClose(rsrc);
+	// Close resource manager
+	viClose(rm);
+
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ с флешки/power_supply_WORK.cpp
@@ -0,0 +1,184 @@
+﻿#include "visa.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+#include <cmath>
+
+//void checkError(long errorStatus) {
+//	if (errorStatus < VI_SUCCESS) {
+//		char errorMessage[256];
+//		viStatusDesc(errorStatus, errorMessage);
+//		fprintf(stderr, "Error: %s\n", errorMessage);
+//		exit(EXIT_FAILURE);
+//	}
+//}
+
+void add_voltage_difference_if_needed (ViSession vi,  float limitU, bool output ){
+	char buf[256] = { 0 };
+	char command[256] = { 0 };
+	long ErrorStatus;
+	float measured_voltage = 0;
+
+	if (output == 0) {
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out1\n");
+	}
+	else {
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+	}
+	
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:VOLT?\n");
+	ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
+	measured_voltage = atof(buf);
+
+	if (measured_voltage < limitU) {
+
+		limitU += limitU - measured_voltage;
+		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU);
+		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+	}
+	else if (measured_voltage > limitU) {
+		
+		limitU -= measured_voltage - limitU;
+		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU);
+		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+	}
+
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:VOLT?\n");
+	ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
+	measured_voltage = atof(buf);
+	printf("Voltage = %f V\n\n", measured_voltage);
+}
+
+int main()
+{
+	ViSession defaultRM, vi;
+	char buf[256] = { 0 };
+	long ErrorStatus;
+
+	float I_offset = 0.36; // A
+	float found_U_offset = NAN; // found offset voltage U 
+
+	//output 1
+	float limitI_offset = 20; //mA
+	float limitU_offset = 1; //V
+	float limitU_offset_changed = limitU_offset;
+	//output 2
+	float limitI_supply = 500; //mA
+	//float limitU_supply = 6.00; //V
+	
+	float measured_current = 0;
+	float measured_voltage = 0;
+	char command[256];
+
+	float step = 0.1;
+	bool stepChanged = false;
+
+	float limitU_supply[] = { 6, 7, 8 };
+	
+
+	viOpenDefaultRM(&defaultRM);
+
+	ErrorStatus = viOpen(defaultRM, (ViPRsrc)"GPIB0::6::INSTR", VI_NULL, VI_NULL, &vi);
+
+
+
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"*RST\n");
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"*IDN?\n");
+	ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
+	printf("Instrument identification string: %s\n", buf);
+
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out1\n");
+	
+	snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset_changed);
+	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+	
+	
+
+	snprintf(command, sizeof(command), "CURR %.2f mA\n", limitI_offset);
+	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+	
+	snprintf(command, sizeof(command), "CURR %.2f mA\n", limitI_supply);
+	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+
+
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+	snprintf(command, sizeof(command), "VOLT 0\n");
+	ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+
+	ErrorStatus = viPrintf(vi, (ViPRsrc)"OUTP ON\n");
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	for (int i = 0; i < 3; i++) {
+
+		printf("======== VOLTAGE SUPPLY = %.0f V ========\n", limitU_supply[i]);
+
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_supply[i]);
+		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+		add_voltage_difference_if_needed(vi, limitU_offset_changed, 0);
+
+		add_voltage_difference_if_needed(vi, limitU_supply[i], 1);
+
+
+		while (1) {
+
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+
+			ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out2\n");
+			ErrorStatus = viPrintf(vi, (ViPRsrc)"MEAS:CURR?\n");
+			ErrorStatus = viScanf(vi, (ViPRsrc)"%t", &buf);
+
+			measured_current = atof(buf);
+			printf("Current = %f A\n", measured_current);
+			if ((measured_current <= I_offset + 0.01) && (measured_current >= I_offset - 0.01)) {
+				found_U_offset = limitU_offset_changed;
+				printf("Last current = %f A\n", measured_current);
+				printf("Found offset voltage = %f V\n\n", found_U_offset);
+				break;
+			}
+
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+
+
+			if ((!stepChanged) && (measured_current > (I_offset + 0.01))) {
+				limitU_offset_changed += step;
+				step = 0.01;
+				stepChanged = true;
+			}
+
+			if (limitU_offset == 0.4) {
+				printf("Last current = %f out of 0.4 voltage\n", measured_current);
+				break;
+			}
+
+			limitU_offset_changed -= step; //0.1
+
+			ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out1\n");
+			snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset_changed);
+			ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+
+			add_voltage_difference_if_needed(vi, limitU_offset_changed, 0);
+
+
+		}
+		ErrorStatus = viPrintf(vi, (ViPRsrc)"inst:sel out1\n");
+		snprintf(command, sizeof(command), "VOLT %.2f\n", limitU_offset);
+		ErrorStatus = viPrintf(vi, (ViPRsrc)command);
+		limitU_offset_changed = limitU_offset;
+		step = 0.1;
+		stepChanged = false;
+	}
+
+	viClose(vi);
+	viClose(defaultRM);
+	return 0;
+}
